471d64a5bb87bc3eb8980dc75adf237392161c64
Michael ALTMAN <altman_mike@yahoo.fr>
2024-04-07 20:22:36 +0200

    Dashboard started

--- src/Controller/DashController.php
+++ src/Controller/DashController.php
@@ -8,29 +8,14 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DashController extends AbstractController
 {
-    #[Route('/dash2', name: 'app_dash2')]
-    public function dash_index(): Response
-    {
-        return $this->render('dashboard/dash_index.html.twig', [
-            'controller_name' => 'DashController',
-        ]);
-    }
-
     #[Route('/dash', name: 'app_dash')]
-    public function dashboard(): Response
+    public function dash(): Response
     {
-        return $this->render('dashboard/dashboard.html.twig', [
-            'controller_name' => 'DashController',
-        ]);
-    }
-
-    #[Route('/dash/budget', name: 'app_dash_budget')]
-    public function dash_budget(): Response
-    {
-        return $this->render('dashboard/budgets/dash_budget.html.twig', [
+        return $this->render('dashboard/dashboard2024.html.twig', [
             'controller_name' => 'DashController',
         ]);
     }
 
     // TODO : Les autres routes du controller Dash
+
 }
